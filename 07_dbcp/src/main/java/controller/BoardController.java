516e888e2ba0c4143bec5d5ca4db161f5687b78d
Bell-212 <helloj0212@gmail.com>
2023-09-19 17:50:30 +0900

    20230919

--- 07_dbcp/src/main/java/controller/BoardController.java
+++ 07_dbcp/src/main/java/controller/BoardController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,20 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.ActionForward;
-import service.MvcService;
-import service.MvcServiceImpl;
+import service.BoardService;
+import service.BoardServiceImpl;
 
 /**
- * Servlet implementation class MvcController
+ * Servlet implementation class BoardController
  */
 @WebServlet("*.do")
-public class MvcController extends HttpServlet {
+public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
   /**
    * @see HttpServlet#HttpServlet()
    */
-  public MvcController() {
+  public BoardController() {
       super();
       // TODO Auto-generated constructor stub
   }
@@ -32,43 +31,48 @@ public class MvcController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-	  // 요청 인코딩 + 응답 타입과 인코딩
-	  request.setCharacterEncoding("UTF-8");
+	  // BoardFilter 실행 후 Controller 실행
+	  
+	  // 요청 인코딩(BoardFilter가 수행함) + 응답 타입과 인코딩
+    // request.setCharacterEncoding("UTF-8");
 	  response.setContentType("text/html; charset=UTF-8");
 	  
-	  // 요청 확인(URLMapping 확인)
-	  String requestURI = request.getRequestURI();                     /*  /mvc/getDate.do */
-	  String contextPath = request.getContextPath();                   /*  /mvc            */
-	  String urlMapping = requestURI.substring(contextPath.length());  /*  /getDate.do     */
+	  // 요청 주소 확인
+	  String requestURI = request.getRequestURI();
+	  String contextPath = request.getContextPath();
+	  String urlMapping = requestURI.substring(contextPath.length());
 	  
-	  // 서비스 객체 생성(MVC Pattern에서 Model에 해당함)
-	  MvcService mvcService = new MvcServiceImpl();
-	  
-	  // 서비스 실행 결과(어디로 어떻게 이동할 것인가에 관한 정보가 저장됨)
+	  // 어디로 어떻게 이동할 것인지 알고 있는 ActionForward 객체
 	  ActionForward af = null;
 	  
-	  // 요청에 따른 서비스 실행
+	  // BoardService 객체 생성
+	  BoardService boardService = new BoardServiceImpl();
+	  
+	  // 요청에 따른 처리
 	  switch(urlMapping) {
-	  case "/getDate.do":
-	    af = mvcService.getDate(request);
+	  // 단순 이동 (forward 처리)
+	  case "/board/list.do":
+	    af = new ActionForward("/board/list.jsp", false);
 	    break;
-	  case "/getTime.do":
-	    af = mvcService.getTime(request);
+	  case "/board/write.do":
+	    af = new ActionForward("/board/write.jsp", false);
 	    break;
-	  case "/getDatetime.do":
-	    mvcService.getDatetime(request, response);
+	  // 서비스 처리
+	  case "/board/register.do":
+	    af = boardService.register(request);
 	    break;
 	  }
 	  
-	  // 서비스 실행 결과에 의한 이동 (redirect와 forward의 선택은 서비스에서 결정함)
+	  // 이동
 	  if(af != null) {
 	    if(af.isRedirect()) {
 	      response.sendRedirect(af.getPath());
 	    } else {
 	      request.getRequestDispatcher(af.getPath()).forward(request, response);
-      
 	    }
 	  }
+	  
+	  
 	  
 	}
 
